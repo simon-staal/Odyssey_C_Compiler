e4393a959dacd30c62ce0f84f08eeb9166de5e92
sts219 <sts219@ic.ac.uk>
2021-03-14 22:39:14 +0000

    Finished ifElse ast, untested

--- include/ast/blocks/ast_ifElse.hpp
+++ include/ast/blocks/ast_ifElse.hpp
@@ -2,7 +2,26 @@
 #define ast_ifElse_hpp
 
 #include "ast/ast_node.hpp"
+#include "ast/blocks/ast_scope.hpp" // Required for exclusive if constructor
 
 class IfElse
+  : public Node
+{
+public:
+  // Constructors
+  IfElse(NodePtr condition, NodePtr ifScope, NodePtr elseScope);
+  IfElse(NodePtr condition, NodePtr ifScope);
+
+  // Destructor
+  virtual ~IfElse();
+
+  // Where things should be
+  NodePtr getCondition() const;
+  NodePtr getIfScope() const;
+  NodePtr getElseScope() const;
+
+  // Visualising
+  virtual void PrettyPrint(std::ostream &dst, std::string indent) const override;
+}
 
 #endif
---
+++ src/include_impl/ast/blocks/ast_ifElse.cpp
@@ -0,0 +1,51 @@
+#include "ast/blocks/ast__ifElse.hpp"
+
+// Constructors
+IfElse::IfElse(NodePtr condition, NodePtr ifScope, NodePtr elseScope)
+{
+  branches.push_back(condition);
+  branches.push_back(ifScope);
+  branches.push_back(elseScope);
+}
+
+IfElse::IfElse(NodePtr condition, NodePtr ifScope)
+  : IfElse(condition, ifScope, new Scope())
+{}
+
+// Destructor
+IfElse::~IfElse()
+{
+  delete branches[0];
+  delete branches[1];
+  delete branches[2];
+}
+
+// Where things should be
+NodePtr IfElse::getCondition() const
+{
+  return branches[0];
+}
+
+NodePtr IfElse::getIfScope() const
+{
+  return branches[1];
+}
+
+NodePtr IfElse::getElseScope() const
+{
+  return branches[2];
+}
+
+// Visualising
+void IfElse::PrettyPrint(std::ostream &dst, std::string indent) const
+{
+  dst << indent << "Condition [" << std::endl;
+  branches[0]->PrettyPrint(dst, indent+"  ");
+  dst << indent << "] endCondition" << std::endl;
+  dst << indent << "If Scope [" << std::endl;
+  branches[1]->PrettyPrint(dst, indent+"  ");
+  dst << indent << "] endIfScope" << std::endl;
+  dst << indent << "Else Scope [" << std::endl;
+  branches[2]->PrettyPrint(dst, indent+"  ");
+  dst << indent << "] endElseScope" << std::endl;
+}
