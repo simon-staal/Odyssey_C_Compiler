b086b4b9eae9ef8c4921a4129b381362a93894fd
sts219 <sts219@ic.ac.uk>
2021-03-18 17:46:30 +0000

    Refactored most scripts into utility (didn't touch kais), should still be ran from base directory

--- utility/add_ast.sh
+++ utility/add_ast.sh
@@ -9,7 +9,7 @@ INCLUDE_DIR="include/ast/"
 SRC_DIR="src/include_impl/ast/"
 
 if [[ $# -eq 0 ]] ; then
-  >&2 echo "Usage: ./add_ast.sh <Construct_Name> <Path>"
+  >&2 echo "Usage: utility/add_ast.sh <Construct_Name> <Path>"
   >&2 echo "<Construct_Name> is the name of the new class to be added to the AST (without prefix), i.e. ifThenElse for ast_ifThenElse"
   >&2 echo "<Path> (optional) is any extra filepath from the ast directory (i.e. \"operators/\" would create file in .../ast/operators/ )"
   exit 1
