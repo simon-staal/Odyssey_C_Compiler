1c7cc69ed9c6aa583f585dfd0220649e3876c826
sts219 <sts219@ic.ac.uk>
2021-03-23 21:38:11 +0000

    Added khayle test - passes

---
+++ compiler_tests/Extra/six_args_khayle.c
@@ -0,0 +1,8 @@
+int g(int x, int y, int z, int a, int b, int c)
+{
+    return x + y + z + a + b + c;
+}
+
+int f() {
+    return g(1,2,3,4,5,6);
+}
---
+++ compiler_tests/Extra/six_args_khayle_driver.c
@@ -0,0 +1,7 @@
+int g(int a, int b, int c, int d, int e, int f);
+int f();
+
+int main()
+{
+    return !(f()==21);
+}
